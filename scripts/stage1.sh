bd87575f833ce5fd5fb11c5bcfe814f07dfffb69
Redson <redson@riseup.net>
2025-11-01 13:42:59 -0300

    Run stage2 script

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -8,3 +8,7 @@ pkg install -y proot-distro
 echo "Installing debian"
 proot-distro install debian
 echo "Debian is installed."
+echo "Downloading and running script inside the proot-distro"
+proot-distro login debian -- curl https://redsonbr140.github.io/termux-whatsberry-installer/scripts/stage2.sh -o stage2.sh
+proot-distro login debian -- chmod +x stage2.sh
+proot-distro login debian -- bash stage2.sh
